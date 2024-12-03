23052ca811de829b3b3e101b86dd99aff86d4911
Bas Verweij <bas@basverweij.nl>
2024-12-03 20:53:20 +0100

    Complete day 1, puzzle 1

---
+++ src/AdventOfCode2024.Day01/Program.cs
@@ -0,0 +1,37 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var locationIds = lines
+    .Select(ParseLine)
+    .ToArray();
+
+var left = locationIds
+    .Select(l => l.left)
+    .Order()
+    .ToArray();
+
+var right = locationIds
+    .Select(l => l.right)
+    .Order()
+    .ToArray();
+
+var puzzle1 = 0;
+
+for ( var i = 0; i < locationIds.Length; i++ )
+{
+    puzzle1 += Math.Abs(left[i] - right[i]);
+}
+
+Console.WriteLine($"Day 1 - Puzzle 1: {puzzle1}");
+
+(int left, int right) ParseLine(
+    string line)
+{
+    var parts = line
+        .Split(
+            ' ',
+            StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    return ( parts[0], parts[1] );
+}
