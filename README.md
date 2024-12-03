32963abfba78e136e6a00b642ab254c76846f34b
Bas Verweij <bas@basverweij.nl>
2024-12-03 21:23:46 +0100

    Complete day 2, puzzle 1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent of Code 2024
 
 * [Day 1](src/AdventOfCode2024.Day01/README.md)
+* [Day 2](src/AdventOfCode2024.Day02/README.md)
---
+++ src/AdventOfCode2024.Day02/Program.cs
@@ -0,0 +1,33 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var reports = lines
+    .Select(ParseLine)
+    .ToArray();
+
+var puzzle1 = reports.Count(IsSafe);
+
+Console.WriteLine($"Day 2 - Puzzle 1: {puzzle1}");
+
+static int[] ParseLine(
+    string line) =>
+    line.Split(' ')
+        .Select(int.Parse)
+        .ToArray();
+
+static bool IsSafe(
+    int[] report)
+{
+    var deltas = Enumerable
+        .Range(
+            0,
+            report.Length - 1)
+        .Select(i => report[i + 1] - report[i])
+        .ToArray();
+
+    if ( deltas.Any(d => d is 0 or < -3 or > 3) )
+    {
+        return false;
+    }
+
+    return deltas.All(d => d < 0) || deltas.All(d => d > 0);
+}
