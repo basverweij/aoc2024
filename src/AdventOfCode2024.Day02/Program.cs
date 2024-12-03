6b69dcc39f66735bbddd21ecf6040708eff4c7f4
Bas Verweij <bas@basverweij.nl>
2024-12-03 21:38:52 +0100

    Complete day 2, puzzle 2

--- src/AdventOfCode2024.Day02/Program.cs
+++ src/AdventOfCode2024.Day02/Program.cs
@@ -8,6 +8,10 @@ var puzzle1 = reports.Count(IsSafe);
 
 Console.WriteLine($"Day 2 - Puzzle 1: {puzzle1}");
 
+var puzzle2 = reports.Count(IsSafeWithProblemDampener);
+
+Console.WriteLine($"Day 2 - Puzzle 2: {puzzle2}");
+
 static int[] ParseLine(
     string line) =>
     line.Split(' ')
@@ -30,4 +34,33 @@ static bool IsSafe(
     }
 
     return deltas.All(d => d < 0) || deltas.All(d => d > 0);
+}
+
+static bool IsSafeWithProblemDampener(
+    int[] report)
+{
+    if ( IsSafe(report) )
+    {
+        return true;
+    }
+
+    for ( var i = 0; i < report.Length; i++ )
+    {
+        var left =
+            i > 0 ?
+                report[..i] :
+                    [];
+
+        var right =
+            i < report.Length - 1 ?
+                report[( i + 1 )..] :
+                    [];
+
+        if ( IsSafe([..left, ..right,]) )
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
