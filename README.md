dab352dfd39dbe59fd5fd82ad1742b7c7d9a59a6
basverweij <basverweij@users.noreply.github.com>
2024-12-03 21:41:00 +0100

    Day 2 (#2)

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Advent of Code 2024
 
+https://adventofcode.com/2024
+
 * [Day 1](src/AdventOfCode2024.Day01/README.md)
+* [Day 2](src/AdventOfCode2024.Day02/README.md)
---
+++ src/AdventOfCode2024.Day02/Program.cs
@@ -0,0 +1,66 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var reports = lines
+    .Select(ParseLine)
+    .ToArray();
+
+var puzzle1 = reports.Count(IsSafe);
+
+Console.WriteLine($"Day 2 - Puzzle 1: {puzzle1}");
+
+var puzzle2 = reports.Count(IsSafeWithProblemDampener);
+
+Console.WriteLine($"Day 2 - Puzzle 2: {puzzle2}");
+
+static int[] ParseLine(
+    string line) =>
+    line.Split(' ')
+        .Select(int.Parse)
+        .ToArray();
+
+static bool IsSafe(
+    int[] report)
+{
+    var deltas = Enumerable
+        .Range(
+            0,
+            report.Length - 1)
+        .Select(i => report[i + 1] - report[i])
+        .ToArray();
+
+    if ( deltas.Any(d => d is 0 or < -3 or > 3) )
+    {
+        return false;
+    }
+
+    return deltas.All(d => d < 0) || deltas.All(d => d > 0);
+}
+
+static bool IsSafeWithProblemDampener(
+    int[] report)
+{
+    if ( IsSafe(report) )
+    {
+        return true;
+    }
+
+    for ( var i = 0; i < report.Length; i++ )
+    {
+        var left =
+            i > 0 ?
+                report[..i] :
+                    [];
+
+        var right =
+            i < report.Length - 1 ?
+                report[( i + 1 )..] :
+                    [];
+
+        if ( IsSafe([..left, ..right,]) )
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
